7b0b8028baeea351340fde65a6bd725bb9c5c57b
Ceydaay <cydylmz.99@gmail.com>
2024-08-25 00:58:20 +0300

    Add project files.

---
+++ List/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        List<string> ziyaretciİsimleri = new List<string>();
+
+        ziyaretciİsimleri.Add("Bülent Ersoy");
+        ziyaretciİsimleri.Add("Ajda Pekkan");
+        ziyaretciİsimleri.Add("Ebru Gündeş");
+        ziyaretciİsimleri.Add("Hadise");
+        ziyaretciİsimleri.Add("Hande Yener");
+        ziyaretciİsimleri.Add("Tarkan");
+        ziyaretciİsimleri.Add("Funda Arar");
+        ziyaretciİsimleri.Add("Demet Akalın");
+
+        Console.WriteLine("**Davetliler**");
+
+        foreach(string isim in ziyaretciİsimleri)
+        {
+            Console.WriteLine(isim);
+        }
+
+
+
+
+
+    }
+}
